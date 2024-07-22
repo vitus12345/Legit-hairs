713eb17cc1482c780b09c38139494cb02fafaff9
vitus <137714347+vitus12345@users.noreply.github.com>
2024-07-22 19:07:08 +0100

    added carousel

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { Navbar, Page, Page2, Page3 } from './components';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/style.scss';
 
 const Display = () => {
   return (
     <React.Fragment>
       <BrowserRouter>
-      <Navbar />
-      <div className="page1">
-      <Page />
-      </div>
-      <div className="page2">
-      <Page2 />
-      </div>
-      <div className="page3">
-      <Page3 />
-      </div>
+        <Navbar />
+        <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <div className="page1">
+                <Page />
+              </div>
+            </div>
+            <div className="carousel-item">
+              <div className="page2">
+                <Page2 />
+              </div>
+            </div>
+            <div className="carousel-item">
+              <div className="page3">
+                <Page3 />
+              </div>
+            </div>
+          </div>
+        </div>
       </BrowserRouter>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Display
+export default Display;
