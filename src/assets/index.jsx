77c804f0c658c6a51040a238f0eabeec7711117c
vitus <137714347+vitus12345@users.noreply.github.com>
2024-07-23 16:46:18 +0100

    comments update

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,20 +1,22 @@
-import Desktop from "./Desktop - 2.png";
-import Desktop1 from "./Desktop - 3.png";
-import Desktop2 from "./Desktop-2k.png";
-import Desktop3 from "./Desktop-3h.png";
-import Group from "./Group 68.png";
-import Ellipse from "./Ellipse 1.png";
-import Ellipse1 from "./Ellipse 2.png";
-import Ellipse2 from "./Ellipse 3.png";
-import Logo from "./Logo.png";
-import Arrow from "./Vector.png";
-import Vector from "./Vector 21.png";
-import Vector1 from "./Vector 22.png";
-import Vector2 from "./Vector 23.png";
-import PlayStore from "./Play-store.png";
-import AppleStore from "./App-store.png";
-import Line from "./Line1.png";
+// Import images from the local directory
+import Desktop from "./Desktop - 2.png"; // Import image for Desktop view 2
+import Desktop1 from "./Desktop - 3.png"; // Import image for Desktop view 3
+import Desktop2 from "./Desktop-2k.png"; // Import image for Desktop view 2k
+import Desktop3 from "./Desktop-3h.png"; // Import image for Desktop view 3h
+import Group from "./Group 68.png"; // Import image for Group 68
+import Ellipse from "./Ellipse 1.png"; // Import image for Ellipse 1
+import Ellipse1 from "./Ellipse 2.png"; // Import image for Ellipse 2
+import Ellipse2 from "./Ellipse 3.png"; // Import image for Ellipse 3
+import Logo from "./Logo.png"; // Import image for Logo
+import Arrow from "./Vector.png"; // Import image for Arrow vector
+import Vector from "./Vector 21.png"; // Import image for Vector 21
+import Vector1 from "./Vector 22.png"; // Import image for Vector 22
+import Vector2 from "./Vector 23.png"; // Import image for Vector 23
+import PlayStore from "./Play-store.png"; // Import image for Play Store icon
+import AppleStore from "./App-store.png"; // Import image for Apple Store icon
+import Line from "./Line1.png"; // Import image for Line 1
 
+// Export all images for use in other parts of the application
 export {
   Desktop,
   Desktop1,
