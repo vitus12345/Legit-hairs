730bc3ee2044f18c6eb6045c7eb33ca60de08db1
vitus <137714347+vitus12345@users.noreply.github.com>
2024-07-24 10:01:42 +0100

    bug fixes

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,9 +1,4 @@
 // Import images from the local directory
-import Desktop from "./Desktop - 2.png"; // Import image for Desktop view 2
-import Desktop1 from "./Desktop - 3.png"; // Import image for Desktop view 3
-import Desktop2 from "./Desktop-2k.png"; // Import image for Desktop view 2k
-import Desktop3 from "./Desktop-3h.png"; // Import image for Desktop view 3h
-import Group from "./Group 68.png"; // Import image for Group 68
 import Ellipse from "./Ellipse 1.png"; // Import image for Ellipse 1
 import Ellipse1 from "./Ellipse 2.png"; // Import image for Ellipse 2
 import Ellipse2 from "./Ellipse 3.png"; // Import image for Ellipse 3
@@ -18,11 +13,6 @@ import Line from "./Line1.png"; // Import image for Line 1
 
 // Export all images for use in other parts of the application
 export {
-  Desktop,
-  Desktop1,
-  Desktop2,
-  Desktop3,
-  Group,
   Ellipse,
   Ellipse1,
   Ellipse2,
